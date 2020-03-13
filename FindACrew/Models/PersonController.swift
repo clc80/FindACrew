5bdcec7a27ed7e5c8c3cb76b1e109b005320dfa9
Claudia Maciel <clc_80@yahoo.com>
2020-03-12 20:22:10 -0700

    Person Controller completed

--- FindACrew/Models/PersonController.swift
+++ FindACrew/Models/PersonController.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 class PersonController {
-    private let baseURL = URL(string: "https://swapi.co/api/people")!
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    private let baseURL = URL(string: "https://swapi.co/api/people/")!
     
     func searchForPeopleWith(searchTerm: String, completion: @escaping ([Person]) -> Void) {
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
@@ -22,5 +30,30 @@ class PersonController {
             return
         }
         var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                print("Error fetching data: \(error!)")
+                completion([])
+                return
+            }
+            
+            guard let data = data else {
+                print("Error: No data returned from data task.")
+                completion([])
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let personSearch = try jsonDecoder.decode(PersonSearch.self, from: data)
+                completion(personSearch.results)
+            } catch {
+                print("Unable to decode data: \(error)")
+                completion([])
+            }
+            
+        }.resume()
     }
 }
