13ea4b58eca802072de81b9a3df0ac9e6b67c979
Claudia Maciel <clc_80@yahoo.com>
2020-03-12 20:05:47 -0700

    Created Person Model and Person Controller

--- FindACrew/Models/PersonController.swift
+++ FindACrew/Models/PersonController.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class PersonController {
+    private let baseURL = URL(string: "https://swapi.co/api/people")!
+    
+    func searchForPeopleWith(searchTerm: String, completion: @escaping ([Person]) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            print("Error: Request URL is nil")
+            completion([Person]())
+            return
+        }
+        var request = URLRequest(url: requestURL)
+    }
+}
