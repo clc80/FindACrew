2fe2ebaedaa252d4490714937ad4406220b1fb66
Claudia Maciel <clc_80@yahoo.com>
2020-03-12 18:56:56 -0700

    Organized files

---
+++ FindACrew/Models/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  FindACrew
+//
+//  Created by Claudia Contreras on 3/12/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
---
+++ FindACrew/Models/PersonController.swift
@@ -0,0 +1,9 @@
+//
+//  PersonController.swift
+//  FindACrew
+//
+//  Created by Claudia Contreras on 3/12/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
